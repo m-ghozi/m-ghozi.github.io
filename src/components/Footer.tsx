d7bc422bf4ef20db58392505233b1a8aae424dee
M. Ghozi Syah Putra <ghozi286@gmail.com>
2025-09-22 11:21:06 +0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
 		<div id="footer" className="flex w-full justify-center bg-verdant-bg lg:fixed lg:bottom-10">
 			<div className="mx-auto flex justify-center">
 				<span className="px-4 py-4 text-center text-verdant-fg lg:px-0">
-					©2024 <span className="text-verdant-blue">M. Ghozi Syah Putra</span>. All Rights Reserved.
+					©2025 <span className="text-verdant-blue">M. Ghozi Syah Putra</span>. All Rights Reserved.
 				</span>
 			</div>
 		</div>
